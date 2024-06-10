f541619f2db3e5a078c79d52cdb27750e1f09d02
Roman <hiimchroman@gmail.com>
2024-06-10 20:19:28 +0200

    ui hlavneho menu

--- app/src/main/java/com/example/milionar/MainMenu.kt
+++ app/src/main/java/com/example/milionar/MainMenu.kt
@@ -3,13 +3,26 @@ package com.example.milionar
 import android.content.res.Resources.Theme
 import android.os.Build
 import androidx.annotation.RequiresApi
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.material.MaterialTheme
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.layout.wrapContentHeight
+import androidx.compose.foundation.layout.wrapContentWidth
+import androidx.compose.material3.CheckboxDefaults
+import androidx.compose.material.Scaffold
+import androidx.compose.material.TopAppBar
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Button
 import androidx.compose.material3.Checkbox
+
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
@@ -20,9 +33,13 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.tooling.preview.PreviewParameter
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.lifecycle.ViewModel
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
@@ -84,12 +101,10 @@ fun MilionarApp(
             )
         }
         composable(MainMenu.ScoreBoard.name) {
-            ScoreboardScreen(viewModel.scoreboard,navController,viewModel)
+            ScoreboardScreen(viewModel.scoreboard, navController, viewModel)
         }
     }
 }
-
-
 
 @Composable
 fun MainMenuScreen(
@@ -101,62 +116,107 @@ fun MainMenuScreen(
     onMenuClick: () -> Unit,
     onScoreBoardClick: () -> Unit
 ) {
-    Button(onClick = { onScoreBoardClick() }) {
-    Text(text = "Zobraz scoreboard")
+    val peachPink = Color(0xFFFFDAB9)
+    Scaffold() { innerPadding ->
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(innerPadding)
+                .background(peachPink),
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.Center
+        ) {
+            Column(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .padding(16.dp),
+                horizontalAlignment = Alignment.CenterHorizontally,
+                verticalArrangement = Arrangement.Center
+            ) {
+
+                Text(
+                    text = "Ako sa stat milionarom?",
+                    fontSize = 28.sp,
+                    style = MaterialTheme.typography.h5.copy(fontWeight = FontWeight.Bold),
+                    modifier = Modifier
+                        .padding(bottom = 8.dp)
+                        .fillMaxWidth()
+                    //.wrapContentWidth(Alignment.CenterHorizontally)
+                )
+
+                Text("Kviz", style = MaterialTheme.typography.h6)
+
+                Spacer(modifier = Modifier.height(32.dp)) // Increased spacing
+
+                Button(
+                    onClick = onStartGameClick, modifier = Modifier.fillMaxWidth(),
+                    colors = ButtonDefaults.buttonColors(Color.hsl(120f, 0.5f, 0.5f))
+                ) {
+                    Text("Zacni kviz", style = TextStyle(fontWeight = FontWeight.Bold))
+                }
+
+                Spacer(modifier = Modifier.height(16.dp))
+
+                // Theme Selection Button
+                Button(
+                    onClick = onSelectThemeClick, modifier = Modifier.fillMaxWidth(),
+                    colors = ButtonDefaults.buttonColors(Color.hsl(27.2f, 0.733f, 0.367f))
+                ) {
+                    Text("Zvol temu", style = TextStyle(fontWeight = FontWeight.Bold))
+                }
+
+                Spacer(modifier = Modifier.height(16.dp))
+
+                // Scoreboard Button
+                Button(
+                    onClick = onScoreBoardClick, modifier = Modifier.fillMaxWidth(),
+                    colors = ButtonDefaults.buttonColors(Color.hsl(27.2f, 0.733f, 0.367f))
+                ) {
+                    Text("Zobraz scoreboard", style = TextStyle(fontWeight = FontWeight.Bold))
+                }
+
+                Spacer(modifier = Modifier.height(32.dp))
+
+                // Difficulty Selection
+                Text("Zvol obtiaznost", style = MaterialTheme.typography.h6)
+                DifficultySelection(selectedDifficulty, onDifficultySelected)
+            }
+        }
     }
-    Spacer(modifier = Modifier.height(5.dp))
+}
+
+@Composable
+fun DifficultySelection(
+    selectedDifficulty: String?,
+    onDifficultySelected: (String) -> Unit
+) {
     Column {
-        Text(text = "Menu")
-        Spacer(modifier = Modifier.height(16.dp))
-        TextButton(onClick = {
-            onSelectThemeClick()
-        }) {
-            Text(text = "Zvol temu")
-        }
-        Spacer(modifier = Modifier.height(30.dp))
-        Text(text = "Zvol obtiaznost")
-        Row {
-
-            Row(verticalAlignment = Alignment.CenterVertically) {
-                Checkbox(
-                    checked = selectedDifficulty == "Lahka",
-                    onCheckedChange = { isChecked ->
-                        if (isChecked) onDifficultySelected("Lahka")
-                        else onDifficultySelected("")
-
-                    }
-                )
-                Text("Lahka")
-            }
-            Spacer(modifier = Modifier.width(8.dp))
-            Row(verticalAlignment = Alignment.CenterVertically) {
-                Checkbox(
-                    checked = selectedDifficulty == "Stredna",
-                    onCheckedChange = { isChecked ->
-                        if (isChecked) onDifficultySelected("Stredna")
-                        else onDifficultySelected("")
-                    }
-                )
-                Text("Stredna")
-            }
-            Spacer(modifier = Modifier.width(8.dp))
-            Row(verticalAlignment = Alignment.CenterVertically) {
-                Checkbox(
-                    checked = selectedDifficulty == "Tazka",
-                    onCheckedChange = { isChecked ->
-                        if (isChecked) onDifficultySelected("Tazka")
-                        else onDifficultySelected("")
-                    }
-                )
-                Text("Tazka")
-            }
-        }
-        Spacer(modifier = Modifier.height(30.dp))
-        TextButton(onClick = {
-            onStartGameClick()
-        }) {
-            Text(text = "Zacni kviz")
-        }
+        DifficultyOption(
+            text = "Lahka",
+            selected = selectedDifficulty == "Lahka",
+            onSelected = onDifficultySelected)
+        DifficultyOption(
+            text = "Stredna",
+            selected = selectedDifficulty == "Stredna",
+            onSelected = onDifficultySelected)
+        DifficultyOption(
+            text = "Tazka",
+            selected = selectedDifficulty == "Tazka",
+            onSelected = onDifficultySelected)
     }
+}
 
+@Composable
+fun DifficultyOption(text: String, selected: Boolean, onSelected: (String) -> Unit) {
+    Row(verticalAlignment = Alignment.CenterVertically) {
+        Checkbox(checked = selected, onCheckedChange = { isChecked ->
+            if (isChecked) {
+                onSelected(text)
+            } else {
+                onSelected("")
+            }
+        }, colors = CheckboxDefaults.colors(Color.Black))
+
+        Text(text)
+    }
 }
